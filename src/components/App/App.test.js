675a1feaffa6c36d5c5c0b7ce5666a0d53d48278
ankova <aneta.takucheva@gmail.com>
2020-02-08 14:03:43 +0000

    Fix

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import { mount } from 'enzyme';
-import axios from 'axios';
 
 import App from './App';
 
 describe('App component', () => {
   let component;
-  const movies = [{
-    name: "Joni Baez",
-    genres: ["action", 'triller', 'horror']
-  }];
 
   beforeEach(() => component = mount(<App />));
 
@@ -18,20 +13,6 @@ describe('App component', () => {
     const tree = renderer.create(<App />).toJSON;
     expect(tree).toMatchSnapshot();
   });
-
-  it('tests', () => {
-    axios.get = jest.fn();
-    const setMovies = jest.fn().mock;
-    const test = axios.get.mockImplementation(() => Promise.resolve(movies));
-    // const setMovies = jest.fn().mockImplementation(test);
-    // expect(setMovies).toBeCalled();
-
-    console.log(test())
-    console.log(setMovies)
-    console.log(component.html())
-
-  });
-
 });
 
 
