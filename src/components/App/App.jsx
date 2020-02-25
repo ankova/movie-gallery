b153604c91ff9958c65fe3aed3c399ff5c5a1e72
ankova <aneta.takucheva@gmail.com>
2020-02-25 19:29:33 +0000

    Reduce

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -14,43 +14,32 @@ const App = () => {
   const [minRating, setMinRating] = useState([]);
 
   useEffect(() => {
-    //Fetch all genres and set to state
-     axios.get('https://api.themoviedb.org/3/genre/movie/list?api_key=89c84ad4507eac89b10fd0706f10f092&language=en-US?')
-    .then(response => {
-      if(response.status === 200) {
-      return response.data;
-    }})
-    .then(response => {
-      const genres = response.genres;
-      setGenres(genres)
-
-      //Fetch all now playing movies
+    axios.all([
+      axios.get('https://api.themoviedb.org/3/genre/movie/list?api_key=89c84ad4507eac89b10fd0706f10f092&language=en-US?'),
       axios.get('https://api.themoviedb.org/3/movie/now_playing?api_key=89c84ad4507eac89b10fd0706f10f092&language=en-US&page=1')
-      .then(response => {
-        if(response.status === 200) {
-        return response.data;
-      }})
-      .then(async response => {
-            let results = response.results.map(movie =>
-                ({ 
-                  ...movie,  
-                  poster_path: `https://image.tmdb.org/t/p/w500${movie.poster_path}`, //adds full path to poster image
-                  //filters all genres by id and adds them to the movie object as a new property
-                  genres: genres
-                    .filter(genre => movie.genre_ids.indexOf(genre.id) !== -1 )
-                    .map(genre => genre.name)
-                })
-              )
-              
-            return (await results) //returns the modified object
+    ])
+    .then(response => {
+      if(response[0].status === 200) {
+        const genres = response[0].data.genres;
+        setGenres(genres);
+      }
+      if(response[1].status === 200) {
+        let results = response[1].data.results.map(movie =>
+          ({ 
+            ...movie,  
+            poster_path: `https://image.tmdb.org/t/p/w500${movie.poster_path}`, //adds full path to poster image
+            //filters all genres by id and adds them to the movie object as a new property
+            genres: genres
+              .filter(genre => movie.genre_ids.indexOf(genre.id) !== -1 )
+              .map(genre => genre.name)
           })
-          
+        );
+
+        return results;
+      }
+    })
       .then(results => setMovies(results)) // adds the modified object to the state
       .catch(error => {console.log(error.message)}) // can be modified to point to an error page if such
-
-    })
-    .catch(error => {console.log(error.message)})
-
   }, []);
 
   useEffect(() => {
